e22f77400ea0cc01eca78eef3d717a3b6b9f4dfe
luannzin <luandaniel966@gmail.com>
2024-01-18 11:28:10 -0300

    fix: change main system language

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={roboto.className}>
         {children}
         <Analytics />
