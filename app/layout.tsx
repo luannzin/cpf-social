60d18128db8a838fa55038ec62e123bc1f6f7559
luannzin <luandaniel966@gmail.com>
2024-01-23 14:27:29 -0300

    fix: change default favicon from website

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link
+          rel="shortcut icon"
+          href="https://wiki.gelzin.com/api/emoji/%F0%9F%92%B3"
+          type="image/x-icon"
+        />
+      </head>
       <body
         className={`${roboto.className} w-screen h-screen overflow-x-hidden bg-white flex items-center justify-center`}
       >
