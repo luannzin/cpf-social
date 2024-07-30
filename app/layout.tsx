d5e8dfe2447b8fb1219d4dea804ef4b65a00ab98
Luan Fabri <luandaniel966@gmail.com>
2024-07-30 10:25:03 -0300

    feat: add new google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "@/styles/globals.css";
 
@@ -159,6 +160,7 @@ export default function RootLayout({
         {/* <Toaster closeButton /> */}
         <Analytics />
       </body>
+      <GoogleAnalytics gaId="G-7QD564C5ZT" />
     </html>
   );
 }
